905880c47d067bc2b16ef3c73eb3063172002963
Michael Salmon <msalmonse@gmail.com>
2019-10-06 21:20:21 +0200

    Initial commit

---
+++ ShoppingList/Models/UserSettings.swift
@@ -0,0 +1,23 @@
+//
+//  UserSettings.swift
+//  ShoppingList
+//
+//  Created by Michael Salmon on 2019-10-06.
+//  Copyright © 2019 mesme. All rights reserved.
+//
+
+import Foundation
+import Combine
+import SwiftUI
+
+final class UserSettings: ObservableObject, Identifiable {
+    let id = UUID()
+    let objectWillChange = ObservableObjectPublisher()
+
+    @UserDefault("ChosenStore", defaultValue: "")
+    var chosenStore: String {
+        willSet { objectWillChange.send() }
+    }
+
+    init() { return }
+}
